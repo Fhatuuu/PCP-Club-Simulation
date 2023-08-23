2f0663d26875c104a28e83f45dc1e6b69f028ead
Fhatuwani Mokwenda <fmokwen@gmail.com>
2023-08-23 22:12:59 +0200

    Changed isOccupied from int type to AtomicInteger type Added locks in get() method

--- src/clubSimulation/GridBlock.java
+++ src/clubSimulation/GridBlock.java
@@ -1,22 +1,27 @@
 package clubSimulation;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 // GridBlock class to represent a block in the club.
 // only one thread at a time "owns" a GridBlock
 
 public class GridBlock {
-	private int isOccupied; 
+	private AtomicInteger isOccupied; 
 	private final boolean isExit;  //is tthis the exit door?
 	private final boolean isBar; //is it a bar block?
 	private final boolean isDance; //is it the dance area?
 	private int [] coords; // the coordinate of the block.
+
+	private Lock lock = new ReentrantLock();
+
 	
 	GridBlock(boolean exitBlock, boolean barBlock, boolean danceBlock) throws InterruptedException {
 		isExit=exitBlock;
 		isBar=barBlock;
 		isDance=danceBlock;
-		isOccupied= -1;
+		isOccupied= new AtomicInteger(-1);
 	}
 	
 	GridBlock(int x, int y, boolean exitBlock, boolean refreshBlock, boolean danceBlock) throws InterruptedException {
@@ -29,19 +34,32 @@ public class GridBlock {
 	public  int getY() {return coords[1];}
 	
 	public  boolean get(int threadID) throws InterruptedException {
-		if (isOccupied==threadID) return true; //thread Already in this block
-		if (isOccupied>=0) return false; //space is occupied
-		isOccupied=threadID;  //set ID to thread that had block
-		return true;
+		lock.lock();
+
+		try{
+			if (isOccupied.get() == threadID){
+				return true;
+			}
+			
+			if (isOccupied.get() >= 0) {
+				return false; // space is occupied
+			}
+
+			return isOccupied.compareAndSet(-1, threadID);
+			
+		} finally {
+			lock.unlock();
+		}
 	}
 		
+	
 	public void release() {
-		isOccupied=-1;
+		isOccupied.set(-1);
 	}
 	
+	
 	public  boolean occupied() {
-		if(isOccupied==-1) return false;
-		return true;
+		return isOccupied.get() >= 0;
 	}
 	
 	public boolean isExit() {
