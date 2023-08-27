46ef2c2fac2159e580ae6685f413f82d7a32e8d4
Fhatuwani Mokwenda <fmokwen@gmail.com>
2023-08-27 13:26:38 +0200

    New class representinf Andre the Barman

---
+++ src/clubSimulation/Andre.java
@@ -0,0 +1,9 @@
+/**
+ * Andre the barman, serves drinks at the bar area. Moves left and right the bar area serving drinks according to who came first.
+ */
+
+package clubSimulation;
+
+public class Andre extends Thread{
+    
+}
